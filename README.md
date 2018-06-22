b29c074ea5f51dde81e197a820a05d32b026efc3
Akos Toth-Mate <akos.toth-mate@skyscanner.net>
2018-06-22 10:42:11 +0200

    gif support

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Slack bot to play around and recreate the awesome dashboard we had back in 2011 at Prezi.
+
+Requirements:
+- node
+- google chrome
+- voices installed
+- youtube-dl
+- mpv
+- giphy api key
+- localtunnel
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const env = require('node-env-file');
 const execFile = require('child_process').execFile;
 const slackbot = require('botkit').slackbot;
+const giphy_api = require('giphy-js-sdk-core');
 
 env(__dirname + '/.env');
 
 const data_dir = __dirname + '/.data/'
 
 const bot_options = {
-  clientId: process.env.clientId,
-  clientSecret: process.env.clientSecret,
+  clientId: process.env.client_id,
+  clientSecret: process.env.client_secret,
   scopes: ['bot'],
   json_file_store: data_dir + 'db/'
 };
@@ -27,9 +28,10 @@ const listen_types = ['ambient', 'direct_message'];
 var playlist = [];
 var playing = false;
 
+const giphy = giphy_api(process.env.giphy_api_key);
+
 function browse(url) {
-  // TODO: implement browser open in fullscreen
-  // TODO: if image, fit to screen
+  execFile('open', ['/Applications/Google Chrome.app', '--args', '--kiosk', url]);
 }
 
 function play(filename) {
@@ -79,13 +81,12 @@ controller.hears(['^(say) (-v) (.*?) (.*)', '^(say) (.*)', '^(mondd) (.*)'], lis
 controller.hears('^vol ([\\d]+)$', listen_types, (bot, msg) => {
   volume = parseInt(msg.match[1]);
   volume = Math.min(Math.max(volume, 0), 10);
-  // TODO: up/down feature too
   execFile('osascript', ['-e', 'set Volume '+ volume]);
 });
 
-controller.hears('^http[^ ]*$', listen_types, (bot, msg) => {
-  // TODO: does not match
+controller.hears('^http[^]', listen_types, (bot, msg) => {
   var url = msg.match[1];
+  console.log('');
   if (url.match(/youtube/i)) {
     queue(url);
   } else {
@@ -96,7 +97,7 @@ controller.hears('^http[^ ]*$', listen_types, (bot, msg) => {
 controller.hears(['^close$', '^exit$', '^clear$', '^stop$'], listen_types, (bot, msg) => {
   playlist = [];
   playing = false;
-  execFile('killall', ['mpv', 'Safari']);
+  execFile('killall', ['mpv', 'Chrome']);
 });
 
 controller.hears('^next$', listen_types, (bot, msg) => {
@@ -104,21 +105,25 @@ controller.hears('^next$', listen_types, (bot, msg) => {
 });
 
 controller.hears('^help$', listen_types, (bot, msg) => {
-  // TODO: reply with available features
+  bot.reply();
+  // TODO
 });
 
 controller.hears('^vb$', listen_types, (bot, msg) => {
   browse('https://player.mediaklikk.hu/playernew/player.php?video=mtv4live&osfamily=OS%20X&browsername=Safari');
-  // TODO: fullscreen
+  // TODO
 });
 
 controller.hears('^gif (.*)', listen_types, (bot, msg) => {
-  // TODO: giphy + fullscreen
-  // TODO: keyword search
+  var keyword = msg.match[1];
+  giphy.search('gifs', {'q': keyword}).then((response) => {
+    if (response.data.length > 0) {
+      var gif = response.data[Math.floor(Math.random()*response.data.length)];
+      execFile('mpv', ['--fs', '--loop=inf', gif.images.original.mp4_url]);
+    }
+  });
 });
 
 controller.hears(['^yt (.*)', '^youtube (.*)'], listen_types, (bot, msg) => {
   queue('ytsearch:'+ msg.match[1]);
 });
-
-// TODO: add meme subtitles
